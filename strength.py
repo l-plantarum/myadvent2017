e38c30e4f299255b0f8b9025810127e6f9d38080
AKIYAMA <jinbeezame@gmail.com>
2017-12-13 21:41:11 +0900

    Update strength.py

--- strength.py
+++ strength.py
@@ -1,18 +1,20 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from gensim.models import word2vec
 import sys
+import codecs
 
 model = word2vec.Word2Vec.load("model")
 
 print("strength")
 
-strength = model.wv.most_similar(positive=[sys.argv[1]], negative=[sys.argv[2]])
+strength = model.wv.most_similar_cosmul(positive=[sys.argv[1]], negative=[sys.argv[2]])
 
 print(strength)
 
 
 print("weak")
-weak = model.wv.most_similar(positive=[sys.argv[2]], negative=[sys.argv[1]])
+weak = model.wv.most_similar_cosmul(positive=[sys.argv[2]], negative=[sys.argv[1]])
 
 print(weak)
